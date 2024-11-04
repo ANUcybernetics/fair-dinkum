b3146f803af830415867dc6e54d272c4af669655
Ben Swift <ben@benswift.me>
2024-11-04 16:51:51 +1100

    add (now-working) tests for the player module

--- test/fair_dinkum/player_test.exs
+++ test/fair_dinkum/player_test.exs
@@ -1,11 +1,12 @@
 defmodule FairDinkum.PlayerTest do
-  use ExUnit.Case
+  use FairDinkum.DataCase
 
+  alias FairDinkum.Game
   alias FairDinkum.Game.Player
 
   describe "player" do
     test "creates with attributes" do
-      {:ok, player} = Api.create(Player, name: "Test Player", type: :human)
+      {:ok, player} = Game.create_player("Test Player", :human)
 
       assert player.name == "Test Player"
       assert player.type == :human
@@ -13,8 +14,8 @@ defmodule FairDinkum.PlayerTest do
     end
 
     test "reads attributes" do
-      {:ok, player} = Api.create(Player, name: "Test Player", type: :human)
-      {:ok, read_player} = Api.get(Player, player.id)
+      {:ok, player} = Game.create_player("Test Player", :human)
+      {:ok, read_player} = Ash.get(Player, player.id)
 
       assert read_player.id == player.id
       assert read_player.name == player.name
@@ -23,15 +24,15 @@ defmodule FairDinkum.PlayerTest do
     end
 
     test "changes name" do
-      {:ok, player} = Api.create(Player, name: "Original Name", type: :human)
-      {:ok, updated_player} = Api.change_name(Player, player.id, name: "New Name")
+      {:ok, player} = Game.create_player("Original Name", :human)
+      {:ok, updated_player} = Game.change_name(player.id, "New Name")
 
       assert updated_player.name == "New Name"
     end
 
     test "updates state" do
-      {:ok, player} = Api.create(Player, name: "Test Player", type: :human)
-      {:ok, updated_player} = Api.put_state(Player, player.id, key: :score, value: 100)
+      {:ok, player} = Game.create_player("Test Player", :human)
+      {:ok, updated_player} = Game.put_state(player.id, :score, 100)
 
       assert updated_player.state.score == 100
     end
