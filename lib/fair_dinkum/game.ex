82e062e24abb8e81f9e1f336a8666b8d98eaf7e4
Ben Swift <ben@benswift.me>
2024-11-04 16:51:27 +1100

    change code_api function name

--- lib/fair_dinkum/game.ex
+++ lib/fair_dinkum/game.ex
@@ -4,7 +4,7 @@ defmodule FairDinkum.Game do
 
   resources do
     resource FairDinkum.Game.Player do
-      define :create, args: [:name, :type]
+      define :create_player, args: [:name, :type], action: :create
       define :change_name, args: [:name]
       define :put_state, args: [:key, :value]
     end
